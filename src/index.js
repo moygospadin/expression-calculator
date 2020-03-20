91ae6b71c83dcbaccdec9359033c0f9baed7f8b2
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-03-21 00:44:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,115 @@ function eval() {
     return;
 }
 
+
+
 function expressionCalculator(expr) {
-    // write your solution here
+    let values = [];
+    let operations = [];
+    let priorities = new Map([
+        ['(', 0],
+        [')', 0],
+        ['+', 1],
+        ['-', 1],
+        ['*', 2],
+        ['/', 2]
+    ]);
+    expr = expr.replace(/[-\+\*\(\)\/]/g, match => ' ' + match + ' ');
+    let expression = expr.trim().split(/ +/g);
+
+    //check balance of brackets
+
+    let brackets = 0;
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] == '(') {
+            brackets++;
+        }
+        if (expression[i] == ')') {
+            brackets--;
+        }
+    }
+    if (brackets != 0) {
+        throw "ExpressionError: Brackets must be paired";
+    }
+
+    //counting
+
+    for (let i = 0; i < expression.length; i++) {
+
+        if (expression[i] == '(') {
+            operations.push(expression[i]);
+            continue;
+        }
+
+        if (priorities.has(expression[i])) {
+            if (operations.length == 0) {
+                operations.push(expression[i]);
+            } else {
+                while (priorities.get(operations[operations.length - 1]) >= priorities.get(expression[i])) {
+
+                    if (operations[operations.length - 1] == '(' && expression[i] == ')') {
+                        operations.pop();
+                        break;
+                    }
+                    let operand2 = parseFloat(values.pop());
+
+                    let operand1 = parseFloat(values.pop());
+
+                    switch (operations.pop()) {
+                        case '+':
+                            values.push(operand1 + operand2);
+                            break;
+                        case '-':
+                            values.push(operand1 - operand2);
+                            break;
+                        case '*':
+                            values.push(operand1 * operand2);
+                            break;
+                        case '/':
+                            {
+                                if (operand2 == 0) {
+                                    throw new "TypeError: Division by zero.";
+                                }
+                                values.push(operand1 / operand2);
+                            }
+                    }
+                }
+                if (expression[i] != ')') {
+                    operations.push(expression[i]);
+                }
+            }
+        } else {
+
+            values.push(expression[i]);
+        }
+    }
+
+    while (operations.length != 0) {
+
+        let operand2 = parseFloat(values.pop());
+        let operand1 = parseFloat(values.pop());
+        switch (operations.pop()) {
+            case '+':
+                values.push(operand1 + operand2);
+                break;
+            case '-':
+                values.push(operand1 - operand2);
+                break;
+            case '*':
+                values.push(operand1 * operand2);
+                break;
+            case '/':
+                {
+                    if (operand2 == 0) {
+                        throw "TypeError: Division by zero.";
+                    }
+
+                    values.push(operand1 / operand2);
+                }
+        }
+    }
+
+    return values.pop();
 }
 
 module.exports = {
